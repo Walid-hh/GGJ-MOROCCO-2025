e3df63f2de10d0c530c17295d8900a5798e2667d
MidSleepin <walidelgoudali@gmail.com>
2025-01-26 00:12:17 +0100

    dash bubble can move too

--- States/player_in_dash_bubble.gd
+++ States/player_in_dash_bubble.gd
@@ -3,7 +3,7 @@ class_name PlayerInDashBubble
 @onready var player: CharacterBody2D = $"../../../Player"
 @onready var dash_bubble: Area2D = $"../../../BubbleSpawner/DashBubble"
 @onready var slime_sprite: AnimatedSprite2D = $"../../SlimeSprite"
-const SPEED := 500
+const SPEED := 100
 const DASH_SPEED := 3000
 var collision : KinematicCollision2D
 var direction : Vector2
@@ -24,8 +24,13 @@ func enter():
 
 func physics_update(_delta : float):
 	if !player.is_on_floor():
-		player.velocity = Vector2(0 , 0)
-	direction = Input.get_vector("move_left" , "move_right" , "move_up" , "move_down")
+		player.velocity = Vector2(0 , -1)
+	var movement_direction = Input.get_axis("move_left" , "move_right")
+	player.velocity.x = movement_direction * SPEED * _delta
+	if Input.is_action_pressed("move_left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_pressed("move_right"):
+		direction = Vector2.RIGHT
 	if Input.is_action_just_pressed("dash") and dash_access == true :
 		_dash(direction , _delta)
 	collision = player.move_and_collide(player.velocity)
@@ -33,7 +38,7 @@ func physics_update(_delta : float):
 		player.playercollision.emit()
 
 func _dash(direction : Vector2 , delta : float) -> void :
-	player.velocity = direction * DASH_SPEED * delta
+	player.velocity = direction * DASH_SPEED  * delta
 	dash_access = false
 	dash_timer.start(0.5)
 
