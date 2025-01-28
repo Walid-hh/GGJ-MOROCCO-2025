2e7f33981434569a53766f4cf3033f6486e1c82f
MidSleepin <walidelgoudali@gmail.com>
2025-01-28 12:02:09 +0100

    tweens

--- Scripts/dash_bubble_empty.gd
+++ Scripts/dash_bubble_empty.gd
@@ -4,14 +4,22 @@ class_name DashBubblleEmpty
 @onready var player: CharacterBody2D = $"../../../../Player"
 @onready var player_normal : State = $"../../../../Player/StateMachine/PlayerNormal"
 @onready var dash_bubble_spawner: Node2D = $"../../.."
-
+var dash_bubble_initial_pos : Vector2
+var tween : Tween
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	dash_bubble.body_entered.connect(_slime_entered_dash_bubble)
-
+	dash_bubble_initial_pos = dash_bubble.position
+	tween = create_tween()
+	var dash_bubble_offset_y = dash_bubble_initial_pos.y - 5
+	tween.tween_property(dash_bubble , "position:y" , dash_bubble_offset_y , 1)
+	tween.tween_property(dash_bubble , "position:y" , dash_bubble_initial_pos.y , 1)
+	tween.set_loops()
 
 func _slime_entered_dash_bubble(_body_that_entered : CharacterBody2D) -> void :
 	print("slime_entered")
+	tween.kill()
+	dash_bubble.position = dash_bubble_initial_pos
 	#removing the signal before changing states
 	dash_bubble.body_entered.disconnect(_slime_entered_dash_bubble)
 	#places the player in the center of the dash_bubble
--- States/bubble_empty.gd
+++ States/bubble_empty.gd
@@ -4,14 +4,22 @@ class_name BubbleEmpty
 @onready var player: CharacterBody2D = $"../../../../Player"
 @onready var player_normal : State = $"../../../../Player/StateMachine/PlayerNormal"
 @onready var bubble_spawner: Node2D = $"../../.."
-
+var tween : Tween
+var bubble_initial_pos : Vector2
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func enter():
 	bubble.body_entered.connect(_slime_entered_bubble)
-
-
+	bubble_initial_pos = bubble.position
+	tween = create_tween()
+	var bubble_offset_y = bubble_initial_pos.y - 5
+	tween.tween_property(bubble , "position:y" , bubble_offset_y , 1)
+	tween.tween_property(bubble , "position:y" , bubble_initial_pos.y , 1)
+	tween.set_loops()
+	
 func _slime_entered_bubble(_body_that_entered : CharacterBody2D) -> void :
 	print("slime_entered")
+	tween.kill()
+	bubble.position = bubble_initial_pos
 	#removing the signal before changing states
 	bubble.body_entered.disconnect(_slime_entered_bubble)
 	#places the player in the center of the bubble
@@ -23,8 +31,6 @@ func _slime_entered_bubble(_body_that_entered : CharacterBody2D) -> void :
 	player_normal.Transitioned.emit(player_normal , "PlayerInBubble")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _reparent_bubble() -> void :
 	bubble.reparent(player , true)
